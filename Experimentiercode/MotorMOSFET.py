f4e3d8a5c76a28ae6018022d2553ddc0976a948d
Finn Saars <finn.saars@haw-hamburg.de>
2024-05-01 17:06:20 +0200

    Code zur Vermessung der Pumpe eingefügt

--- Experimentiercode/MotorMOSFET.py
+++ Experimentiercode/MotorMOSFET.py
@@ -2,6 +2,21 @@
 # Erforderliche Bibliotheken importieren
 import RPi.GPIO as GPIO
 import time
+import keyboard
+
+def on_key_press(key):
+    if key.name == 'esc':  # Hier wird überprüft, ob die Escape-Taste gedrückt wurde
+        print("Programm wird beendet...")
+        # Hier kannst du Aufräumarbeiten oder weitere Aktionen vor dem Beenden ausführen
+        uhrzeigersinn.stop()
+        Prog_Abbruchzeit = time.time()
+        # Berechnen der Programmzeit aus Startzeit und Endzeit
+        Prog_Laufzeit = Prog_Abbruchzeit - Prog_Startzeit 
+        # Ausgeben der gewünschten Messdaten
+        print(f"Tastgrad: {Tastgrad}")
+        print(f"Laufzeit für 100ml: {Prog_Laufzeit}")
+        keyboard.unhook_all()  # Alle Tastaturhaken entfernen, um das Programm zu beenden
+        quit()  # Programm beenden
 
 # Konvention für Pinnummerierung festlegen (BCM bzw. Board)
 GPIO.setmode(GPIO.BCM)
@@ -17,10 +32,12 @@ uhrzeigersinn = GPIO.PWM(17, 50)
 # PWM mit Tastgrad 0% initialisieren
 uhrzeigersinn.start(0)
 
-#Schleife zur PWM
-Tastgrad = 0
-while Tastgrad <= 100:
-    uhrzeigersinn.ChangeDutyCycle(Tastgrad)
-    time.sleep(2)
-    Tastgrad = Tastgrad + 5
-uhrzeigersinn.stop()
+# Start der Pumpe mit gewünschtem Tastgrad
+Tastgrad = 100
+uhrzeigersinn.ChangeDutyCycle(Tastgrad)
+
+#Zeit zum Programmstart
+Prog_Startzeit = time.time()
+
+# Ausführen der oben definierten on_key_press Funktion zum Programmabbruch und Auswertung beim drücken von "esc"
+keyboard.on_press(on_key_press)
