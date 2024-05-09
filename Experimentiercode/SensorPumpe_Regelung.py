315622bc753deedf7626b712c68cd70a4e792bb4
Kion Hansen <kion.hansen@haw-hamburg.de>
2024-05-09 13:15:21 +0200

    neue Steuerung

--- Experimentiercode/SensorPumpe_Regelung.py
+++ Experimentiercode/SensorPumpe_Regelung.py
@@ -1,19 +1,26 @@
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
+import keyboard
 
 
 
-TRIG = 16
+TRIG = 23
 ECHO = 18
+Pout = 17
 anzahl_messwerte = 5
 distances = []
 
+Xo = 0              # Einführung x0 als Startwert
+dX = 0.5            # delta x in cm 
+dXa = ()         	# Aktualwert xa ab nivilierung
+
 def setup():
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)      # von Board auf BCM geändert
     GPIO.setwarnings(False)
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
+    GPIO.setup(Pout, GPIO.OUT)
 
 def distance():
     GPIO.output(TRIG, 0)
@@ -34,9 +41,16 @@ def distance():
 def get_average_distances():
     if len(distances) < anzahl_messwerte:
         return None
+    
+    elif len(distances) = anzahl_messwerte:                         # nach ersten 5 Messungen wird initial Xo aus gesetzt
+        last_mesurements = distances[-anzahl_messwerte:]
+        average_distance = sum(last_mesurements) / anzahl_messwerte
+        Xo = average_distance                                       # Xo
+        return average_distance
     else:
         last_mesurements = distances[-anzahl_messwerte:]
         average_distance = sum(last_mesurements) / anzahl_messwerte
+        dXa = Xo - average_distance                                  # Aktualwer Xa aus durchschnittlicher Änderung
         return average_distance
 
 def loop():
@@ -48,43 +62,34 @@ def loop():
             average_distance = get_average_distances()
             if average_distance is not None:
                 print('average Distance (last 5 measurements): %.2f mm' % average_distance)
-        time.sleep(1)
-
-
+        time.sleep(0.5)                                           #Messinertvall
+def Motor(pwm,tg):
+    Pumpensteuerung = GPIO.PWM(Pout, pwm)
+    Pumpensteuerung.start(0)
+    Pumpensteuerung.ChangeDutyCycle(tg)
+    if dXa >= dX: 
+        Pumpensteuerung.stop()
 def destroy():
     GPIO.cleanup()
 
-if __name__ == "__main__":
-    setup()
-    try:
-        loop()
-    except KeyboardInterrupt:
-        destroy()
+
+setup()
+loop()
+Motor(50,100)
+
+
 
 #### Pumpensteuerung
 
-import RPi.GPIO as GPIO
-import time
- 
-# Konvention für Pinnummerierung festlegen (BCM bzw. Board)
-GPIO.setmode(GPIO.BCM)
-# Warnungen, die das Ausführen des Programms verhindern, wenn
-# Ausgang bereits als OUT deklariert wurde ignorieren
-GPIO.setwarnings(False)
-# Pins als Ausgänge deklarieren
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(25, GPIO.OUT)
- 
-# PWM für Richtungen mit Frequenz festlegen
-uhrzeigersinn = GPIO.PWM(21, 50)
-gegen_uhrzeigersinn = GPIO.PWM(25, 50)
-# PWM mit Tastgrad 0% initialisieren
-uhrzeigersinn.start(0)
-gegen_uhrzeigersinn.start(0)
- 
-uhrzeigersinn.ChangeDutyCycle(100)
-time.sleep(2)
-uhrzeigersinn.stop()
-gegen_uhrzeigersinn.ChangeDutyCycle(100)
-time.sleep(2)
-gegen_uhrzeigersinn.stop()
+# PWM= PulsWeitenModulation für Richtungen mit Frequenz festlegen
+
+
+# PWM mit Tastgrad 0% initialisieren; Wie viel % der Periode angeschaltet sind 
+
+
+# Start der Pumpe mit gewünschtem Tastgrad
+
+
+#Zeit zum Programmstart
+# Prog_Startzeit = time.time()
+
