6b01840d86717880e59a016e93358ecf3ad215c6
Finn Saars <finn.saars@haw-hamburg.de>
2024-05-17 19:49:05 +0200

    Ausführungsskript für Funksteckdose eingefügt und Klassenmethoden erweitert

---
+++ Code_OOP/FUNKSTECKDOSE_Ausfuehrung.py
@@ -0,0 +1,41 @@
+from FUNKSTECKDOSE_Classdefinition import FUNKSTECKDOSE_Steuerung
+import time
+
+# Zeitstempel zum Programmstart
+lasttime_lamps_on   = 0           
+lasttime_lamps_off  = 0          
+lasttime_airpump_on = 0            
+lasttime_airpump_off= 0             
+
+# Definition von Steuerparametern
+lamps_on_time       = 60*60*24*7    # 24/7 an
+lamps_off_time      = 0             # 0 Stunden
+airpump_on_time     = 60*15         # 15 Minuten
+airpump_off_time    = 60*15         # 15 Miuten
+messzeit = 60*60*24*14 # Gesamtversuchszeit/s
+
+# Initialisieren der Klassenobjekte
+Lampen = FUNKSTECKDOSE_Steuerung(pin=17, on_time=lamps_on_time, off_time=lamps_off_time)
+Luftpumpe = FUNKSTECKDOSE_Steuerung(pin=18, on_time=airpump_on_time, off_time=airpump_off_time)
+
+# Starten des Timers für die Ausführungen
+timer = time()
+
+# Hauptschleife
+while timer <= messzeit:
+
+    if (Lampen.status == False) and (timer-lasttime_lamps_off >= lamps_off_time):
+        Lampen.EIN()
+        lasttime_lamps_on = timer
+
+    if (Lampen.status == True) and (timer-lasttime_lamps_on >= lamps_on_time):
+        Lampen.AUS()
+        lasttime_lamps_off = timer
+
+    if (Luftpumpe.status == False) and (timer-lasttime_airpump_off >= airpump_off_time):
+        Luftpumpe.EIN()
+        lasttime_airpump_on = timer
+
+    if (Luftpumpe.status == True) and (timer-lasttime_airpump_on >= airpump_on_time):
+        Lampen.AUS()
+        lasttime_airpump_off = timer
--- Code_OOP/FUNKSTECKDOSE_Classdefinition.py
+++ Code_OOP/FUNKSTECKDOSE_Classdefinition.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 from gpiozero import LED
 import time
+import datetime
 
 # Definition einer Klasse für Objekte welche über Funksteckdosen angesteuert werden
 class FUNKSTECKDOSE_Steuerung:
@@ -10,9 +11,11 @@ class FUNKSTECKDOSE_Steuerung:
     OFF = LED(22) # BCM-Nummer 22 = Pin 15 als Impulssignal für Relais off
     schaltzeit = 0.2 # Wartezeit für zuverlässige Schaltung der Relais
 
-    def __init__(self, pin):
+    def __init__(self, pin, on_time, off_time):
         self.pin = pin # Pin zur Ansteuerung des Relais
         self.LED = LED(self.pin)
+        self.on_time = on_time # Definition der Zeit die das Objekt eingeschaltet bleibt
+        self.off_time = off_time # Definition der Zeit die das Objekt ausgeschaltet bleibt
         self.status = False # Der Status gibt an ob das Objekt an der Fnksteckdose an oder ausgeschaltet ist, zu Beginn ausgeschaltet
         self.setup()
 
@@ -23,7 +26,7 @@ class FUNKSTECKDOSE_Steuerung:
         FUNKSTECKDOSE_Steuerung.ON.on()
         FUNKSTECKDOSE_Steuerung.OFF.on()
 
-    # Definition von Klassenmethoden zu Ein- und Ausschalten der Steckdosen
+    # Definition von Klassenmethoden zum Ein- und Ausschalten der Steckdosen
     def EIN(self):
         self.LED.off()
         FUNKSTECKDOSE_Steuerung.ON.off()
@@ -31,6 +34,11 @@ class FUNKSTECKDOSE_Steuerung:
         self.LED.on()
         FUNKSTECKDOSE_Steuerung.ON.off()
         self.status = True # Ändern des Status des Objekts zu Eingeschaltet
+        # Codeblock zur Ausgabe der Änderung zum eingeschalteten Zustand im Command Fenster
+        now = datetime.datetime.now() # aktuelles Datum und Zeit
+        date_time = now.strftime("%Y-%m-%d, %H:%M:%S") # Zeitstempel zu dem das Objekt geschaltet wird
+        Object_Name = self.__class__.__name__ # Name des Klassenobjekts
+        print(f"{date_time}: {Object_Name} zeitgesteuert eingeschaltet nach {(self.off_time)/60:5.1f} Minuten")
     
     def AUS(self):
         self.LED.off()
@@ -39,3 +47,8 @@ class FUNKSTECKDOSE_Steuerung:
         self.LED.on()
         FUNKSTECKDOSE_Steuerung.OFF.on()
         self.status = False # Ändern des Status des Objekts zu Ausgeschaltet
+        # Codeblock zur Ausgabe der Änderung zum ausgeschalteten Zustand im Command Fenster
+        now = datetime.datetime.now() # aktuelles Datum und Zeit
+        date_time = now.strftime("%Y-%m-%d, %H:%M:%S") # Zeitstempel zu dem das Objekt geschaltet wird
+        Object_Name = self.__class__.__name__ # Name des Klassenobjekts
+        print(f"{date_time}: {Object_Name} zeitgesteuert ausgeschaltet nach {(self.on_time)/60:5.1f} Minuten")
