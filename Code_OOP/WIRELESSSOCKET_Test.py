8d8a3f414e4d8629da8f8f9f43d77493bb8eb6f7
Finn Saars <finn.saars@haw-hamburg.de>
2024-06-04 14:18:56 +0200

    Skripte zum Testen aller Klassen erstellt

--- Code_OOP/WIRELESSSOCKET_Test.py
+++ Code_OOP/WIRELESSSOCKET_Test.py
@@ -1,30 +1,41 @@
 from WIRELESSSOCKET_Classdefinition import WIRELESSSOCKET_control     
+import time
+
+# Initialparameter der Objekte mit Funksteckdose
+lasttime_lamps_on = 0
+lasttime_lamps_off = 0
+lasttime_airpump_on = 0
+lasttime_airpump_off = 0
 
 # Testparameter
 lamps_on_time = 15
 lamps_off_time = 15
 airpump_on_time = 30
 airpump_off_time = 30
+test_time = 2*60
 
 # Initialisieren der Klassenobjekte
-Lampen = WIRELESSSOCKET_control(pin=17, on_time=lamps_on_time, off_time=lamps_off_time)
-Luftpumpe = WIRELESSSOCKET_control(pin=18, on_time=airpump_on_time, off_time=airpump_off_time)
+lamps = WIRELESSSOCKET_control(pin=17, on_time=lamps_on_time, off_time=lamps_off_time)
+airpump = WIRELESSSOCKET_control(pin=18, on_time=airpump_on_time, off_time=airpump_off_time)
+
+# Starten des Timers für die Ausführungen
+timer = time()
 
 # Hauptschleife
-while timer <= messzeit:
+while timer <= test_time:
 
-    if (Lampen.status == False) and (timer-lasttime_lamps_off >= lamps_off_time):
-        Lampen.EIN()
+    if (lamps.status == False) and (timer-lasttime_lamps_off >= lamps_off_time):
+        lamps.EIN()
         lasttime_lamps_on = timer
 
-    if (Lampen.status == True) and (timer-lasttime_lamps_on >= lamps_on_time):
-        Lampen.AUS()
+    if (lamps.status == True) and (timer-lasttime_lamps_on >= lamps_on_time):
+        lamps.AUS()
         lasttime_lamps_off = timer
 
-    if (Luftpumpe.status == False) and (timer-lasttime_airpump_off >= airpump_off_time):
-        Luftpumpe.EIN()
+    if (airpump.status == False) and (timer-lasttime_airpump_off >= airpump_off_time):
+        airpump.EIN()
         lasttime_airpump_on = timer
 
-    if (Luftpumpe.status == True) and (timer-lasttime_airpump_on >= airpump_on_time):
-        Lampen.AUS()
+    if (airpump.status == True) and (timer-lasttime_airpump_on >= airpump_on_time):
+        airpump.AUS()
         lasttime_airpump_off = timer
